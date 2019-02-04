552b16b7e6a307f2843f23000e85a741721127ba
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-02-04 22:06:35 +0100

    Home endpoint test

--- test/HomeControllerSpec.scala
+++ test/HomeControllerSpec.scala
@@ -1,21 +1,23 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
+import play.api.mvc
 import play.api.test._
 import play.api.test.Helpers._
 
+import scala.concurrent.Future
+
 @RunWith(classOf[JUnitRunner])
-class IntegrationSpec extends Specification {
+class HomeControllerSpec extends Specification {
 
   "Application" should {
 
     "send 404 on a bad request" in new WithApplication {
-      route(app, FakeRequest(GET, "/boum")) must beSome.which (status(_) == NOT_FOUND)
+      route(app, FakeRequest(GET, "/wrong")) must beSome.which (status(_) == NOT_FOUND)
     }
 
-    "render the index page" in new WithApplication {
-      val home = route(app, FakeRequest(GET, "/")).get
+    "get the home endpoint" in new WithApplication {
+      val home: Future[mvc.Result] = route(app, FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
