eea989692c69e8ee35deeba14d65f343a322a848
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-02-05 22:53:19 +0100

    Updated player controller with new repository

--- app/controllers/PlayerController.scala
+++ app/controllers/PlayerController.scala
@@ -1,9 +1,9 @@
 package controllers
 
 import javax.inject._
-import models._
 import play.api.libs.json.Json
 import play.api.mvc._
+import repository.PlayerRepository
 
 import scala.concurrent.ExecutionContext
 
@@ -12,7 +12,7 @@ class PlayerController @Inject()
   (repository: PlayerRepository, cc: MessagesControllerComponents )(implicit ec: ExecutionContext) extends MessagesAbstractController(cc) {
 
   def getAllPlayers: Action[AnyContent] = Action.async { implicit request =>
-    repository.list().map { players =>
+    repository.getAllPlayers().map { players =>
       Ok(Json.toJson(players))
     }
   }
