b9a7f9d693a177f827f3e3cf01e8787176045fcb
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-02-07 20:08:09 +0100

    Updated repositories with one to one relationship

--- app/repository/MatchRepository.scala
+++ app/repository/MatchRepository.scala
@@ -13,19 +13,22 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class MatchRepository @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
 
-  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
+  val playersRepository = new PlayerRepository(dbConfigProvider)
 
   import dbConfig._
   import profile.api._
 
-  private class MatchesTable(tag: Tag) extends Table[Match](tag, "matches") {
+  class MatchesTable(tag: Tag) extends Table[Match](tag, "matches") {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-    def startDate = column[Timestamp]("startDate")
-    def endDate = column[Timestamp]("endDate")
-    def * = (id, startDate, endDate) <> ((Match.apply _).tupled, Match.unapply)
+    def startDate = column[Timestamp]("start_date")
+    def endDate = column[Timestamp]("end_date")
+    def playerId = column[Long]("player_id")
+    def * = (id, startDate, endDate, playerId) <> ((Match.apply _).tupled, Match.unapply)
+    def player = foreignKey("player", playerId, playersRepository.players)(_.id)
   }
 
-  private val matches = TableQuery[MatchesTable]
+  val matches = TableQuery[MatchesTable]
 
   def getAllMatches(): Future[Seq[Match]] = db.run {
     matches.result
--- app/repository/PlayerRepository.scala
+++ app/repository/PlayerRepository.scala
@@ -10,19 +10,19 @@ import scala.concurrent.{ExecutionContext, Future}
 @Singleton
 class PlayerRepository @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
 
-  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
 
   import dbConfig._
   import profile.api._
 
-  private class PlayersTable(tag: Tag) extends Table[Player](tag, "players") {
+  class PlayersTable(tag: Tag) extends Table[Player](tag, "players") {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
-    def firstName = column[String]("firstName")
-    def lastName = column[String]("lastName")
+    def firstName = column[String]("first_name")
+    def lastName = column[String]("last_name")
     def * = (id, firstName, lastName) <> ((Player.apply _).tupled, Player.unapply)
   }
 
-  private val players = TableQuery[PlayersTable]
+  val players = TableQuery[PlayersTable]
 
   def getAllPlayers(): Future[Seq[Player]] = db.run {
     players.result
