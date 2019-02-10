210e835ca7546f060ce7c249f6466539121d8cbb
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-02-10 20:10:35 +0100

    New match endpoints

--- app/controller/MatchController.scala
+++ app/controller/MatchController.scala
@@ -1,6 +1,7 @@
 package controller
 
 import javax.inject.{Inject, Singleton}
+import model.{Match, Player}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, MessagesAbstractController, MessagesControllerComponents}
 import repository.MatchRepository
@@ -34,6 +35,22 @@ class MatchController @Inject()
     repository.getMatchByIdWithPlayer(matchId).map {
       case None => NotFound(s"Match [id = $matchId] not found")
       case Some(game) => Ok(Json.toJson(game))
+    }
+  }
+
+  def saveMatch(): Action[AnyContent] = Action.async { implicit request =>
+    val matchJson = request.body.asJson.get.toString()
+    val matchObject = Match.parseMatchJson(matchJson)
+
+    repository.saveMatch(matchObject.playerId, matchObject.startDate, matchObject.endDate).map(game =>
+      Ok(s"Match [$game] saved")
+    )
+  }
+
+  def deleteMatchById(matchId: Long): Action[AnyContent] = Action.async { implicit request =>
+    repository.deleteMatchById(matchId).map {
+      case 0 => NotFound(s"Match [id = $matchId] not found")
+      case 1 => Ok(s"Match [id = $matchId] deleted")
     }
   }
 }
