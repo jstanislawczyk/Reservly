cc50e63453d84b84016d95be719beb7f6fd1a24a
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-02-17 20:49:10 +0100

    Tabulation change

--- app/controller/HomeController.scala
+++ app/controller/HomeController.scala
@@ -7,10 +7,10 @@ import io.swagger.annotations.{Api, ApiResponse, ApiResponses}
 @Api("HomeController")
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-    @ApiResponses(Array(
-      new ApiResponse(code = 200, message = "Default home endpoint. Returns 'home' message")
-    ))
-    def home(): Action[AnyContent] = Action {
-      Ok("Home")
-    }
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "Default home endpoint. Returns 'home' message")
+  ))
+  def home(): Action[AnyContent] = Action {
+    Ok("Home")
+  }
 }
