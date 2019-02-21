c18e69504d0ee9578c9220dfaa2cb68afd8d1a95
xenonso <jakubstanislawczyk98@gmail.com>
2019-02-21 11:40:17 +0100

    Refactor of broadcast method

--- app/controller/ChatController.scala
+++ app/controller/ChatController.scala
@@ -4,7 +4,7 @@ import actorRegister.GlobalChatActorRegister
 import akka.actor.ActorSystem
 import io.swagger.annotations.{Api, ApiResponse, ApiResponses}
 import javax.inject.Inject
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import play.api.mvc._
 
 @Api("ChatController")
 class ChatController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem) extends AbstractController(cc) {
@@ -13,11 +13,15 @@ class ChatController @Inject()(cc: ControllerComponents, actorSystem: ActorSyste
     new ApiResponse(code = 200, message = "Broadcast given message to global chat")
   ))
   def sendMessageToGlobalChat(): Action[AnyContent] = Action { implicit request =>
+    broadcastMessage(request)
+
+    Ok("")
+  }
+
+  private def broadcastMessage(request: Request[AnyContent]): Unit = {
     val message = request.body.asJson.get.toString()
     val globalChatActorRegister = new GlobalChatActorRegister(actorSystem)
 
     globalChatActorRegister.broadcastMessage(message)
-
-    Ok("")
   }
 }
