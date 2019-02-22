d8976872f9a7f30680b5e518c9e699390fb1882a
PGS-SOFT\jstanislawczyk <jstanislawczyk@pgs-soft.com>
2019-02-22 10:00:36 +0100

    Service layer for chat

--- app/controller/ChatController.scala
+++ app/controller/ChatController.scala
@@ -1,27 +1,22 @@
 package controller
 
-import actorRegister.GlobalChatActorRegister
 import akka.actor.ActorSystem
 import io.swagger.annotations.{Api, ApiResponse, ApiResponses}
 import javax.inject.Inject
 import play.api.mvc._
+import service.ChatService
 
 @Api("ChatController")
 class ChatController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem) extends AbstractController(cc) {
+
+  private val chatService = new ChatService()
 
   @ApiResponses(Array(
     new ApiResponse(code = 200, message = "Broadcast given message to global chat")
   ))
   def sendMessageToGlobalChat(): Action[AnyContent] = Action { implicit request =>
-    broadcastMessage(request)
+    chatService.sendMessageToGlobalChat(actorSystem, request)
 
     Ok("")
-  }
-
-  private def broadcastMessage(request: Request[AnyContent]): Unit = {
-    val message = request.body.asJson.get.toString()
-    val globalChatActorRegister = new GlobalChatActorRegister(actorSystem)
-
-    globalChatActorRegister.broadcastMessage(message)
   }
 }
