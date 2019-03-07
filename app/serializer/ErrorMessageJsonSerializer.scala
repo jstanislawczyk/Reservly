dec548933bc3eecc3caf15815c6e62730cc687cb
PGS-SOFT\jstanislawczyk <jstanislawczyk@pgs-soft.com>
2019-03-07 09:46:31 +0100

    Response message model changed

--- app/serializer/ErrorMessageJsonSerializer.scala
+++ app/serializer/ErrorMessageJsonSerializer.scala
@@ -1,16 +1,16 @@
 package serializer
 
 import com.google.gson.Gson
-import model.ErrorMessage
+import model.ResponseMessage
 
 object ErrorMessageJsonSerializer {
   private val gson = new Gson
 
-  def toJson(errorMessage: ErrorMessage): String = {
+  def toJson(errorMessage: ResponseMessage): String = {
     gson.toJson(errorMessage)
   }
 
-  def fromJson(errorMessageJson: String): ErrorMessage = {
-    gson.fromJson(errorMessageJson, classOf[ErrorMessage])
+  def fromJson(errorMessageJson: String): ResponseMessage = {
+    gson.fromJson(errorMessageJson, classOf[ResponseMessage])
   }
 }
