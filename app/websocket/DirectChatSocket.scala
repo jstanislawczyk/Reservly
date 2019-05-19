f60c1d39d1612ccfc76ba2d87e3e384936c7492d
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-05-19 22:07:34 +0200

    Method for getting request data

--- app/websocket/DirectChatSocket.scala
+++ app/websocket/DirectChatSocket.scala
@@ -7,7 +7,7 @@ import akka.stream.Materializer
 import io.swagger.annotations.{Api, ApiOperation, ApiResponse, ApiResponses}
 import javax.inject.Inject
 import play.api.libs.streams.ActorFlow
-import play.api.mvc.{MessagesAbstractController, MessagesControllerComponents, WebSocket}
+import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
 
@@ -23,7 +23,7 @@ class DirectChatSocket @Inject()
   @ApiResponses(Array(
     new ApiResponse(code = 200, message = "Opens direct chat websocket connection for given chat id")
   ))
-  def directChat(id: String): WebSocket = WebSocket.accept[String, String] { _ =>
+  def directChat(id: String): WebSocket = WebSocket.accept[String, String] { implicit request =>
     ActorFlow.actorRef(out => {
       registerNewActor(actorSystem, id, out.path.toString)
       DirectChatActor.props(out, actorSystem)
@@ -33,5 +33,9 @@ class DirectChatSocket @Inject()
   private def registerNewActor(actorSystem: ActorSystem, actorId: String, actorPath: String): Unit = {
     val directChatActorRegister = new DirectChatActorRegister(actorSystem)
     directChatActorRegister.registerNewActor(actorId, actorPath)
+  }
+
+  private def getPlayerAuthId(request: MessagesRequest[AnyContent]): String = {
+    request.headers.get("Auth-Id").getOrElse("0").toString
   }
 }
