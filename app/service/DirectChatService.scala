3979b3c45805c615be8d920220d53f12c3327a7b
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-05-21 21:29:23 +0200

    New methods for service

--- app/service/DirectChatService.scala
+++ app/service/DirectChatService.scala
@@ -1,14 +1,61 @@
 package service
 
+import actorRegister.DirectChatActorRegister
 import akka.actor.ActorSystem
 import javax.inject.Inject
+import model.DirectChatMessage
+import play.api.mvc.{AnyContent, Request}
 import repository.PlayerRepository
+import serializer.DirectChatMessageJsonSerializer
+import validation.direct_chat_message.DirectChatMessageValidator
 
 import scala.concurrent.ExecutionContext
 
 class DirectChatService @Inject() (playerRepository: PlayerRepository, actorSystem: ActorSystem) (implicit ec: ExecutionContext) {
 
-  def areGivenPlayersValid(senderId: String, receiverId: String): Boolean = {
-    true
+  def sendDirectMessage(actorSystem: ActorSystem, request: Request[AnyContent], chatMembersIds: (String, String)): Boolean = {
+    val chatMessage = DirectChatMessageJsonSerializer.fromJson(request.body.asJson.get.toString())
+    val isMessageValid = DirectChatMessageValidator.validate(chatMessage)
+
+    if(isMessageValid) {
+      val directChatRoomId = buildDirectChatRoomId(chatMembersIds)
+      sendMessage(actorSystem, chatMessage, directChatRoomId)
+    }
+
+    isMessageValid
+  }
+
+  def isGivenPlayerInvalid(userId: String): Boolean = {
+    false
+  }
+
+  def areGivenPlayersInvalid(chatMembersIds: (String, String)): Boolean = {
+    false
+  }
+
+
+  def buildDirectChatRoomId(chatMembersIds: (String, String)): String = {
+
+    /*
+      The direct chat room id is created using the identifiers of both participants in the conversation
+      in a such way that both ids are ordered in alphabetical order and then they are concatenated
+      E.g. some_id, scala_java.
+    */
+
+    var sortedChatMembersIds: (String, String) = chatMembersIds
+
+    if (chatMembersIds._2 > chatMembersIds._1) {
+      sortedChatMembersIds = chatMembersIds.swap
+    }
+
+    s"${sortedChatMembersIds._1}_${sortedChatMembersIds._2}"
+  }
+
+  private def sendMessage(actorSystem: ActorSystem, chatMessage: DirectChatMessage, directChatRoomId: String): Unit = {
+    val directChatActorRegister = new DirectChatActorRegister(actorSystem)
+    val directChatMessageObjectAsJson = DirectChatMessageJsonSerializer.toJson(chatMessage)
+    val chatMessageAsJson = s"[DirectChat: $directChatRoomId] $directChatMessageObjectAsJson"
+
+    directChatActorRegister.sendMessage(chatMessageAsJson, chatMessageAsJson)
   }
 }
