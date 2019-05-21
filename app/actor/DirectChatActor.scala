144812f8038d8d8c47a2a9e921f35e70b1965246
Jakub Stanislawczyk <jakubstanislawczyk98@gmail.com>
2019-05-21 21:29:40 +0200

    Poison pill for invalid user

--- app/actor/DirectChatActor.scala
+++ app/actor/DirectChatActor.scala
@@ -1,15 +1,24 @@
 package actor
 
 import actorRegister.DirectChatActorRegister
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, PoisonPill, Props}
+import service.DirectChatService
 
 object DirectChatActor {
-  def props(out: ActorRef, actorSystem: ActorSystem) = Props(new GlobalChatActor(out, actorSystem))
+  def props(out: ActorRef, actorSystem: ActorSystem, userId: String, directChatService: DirectChatService) = Props(new DirectChatActor(out, actorSystem, userId, directChatService))
+
+  var userId: String = _
 }
 
-class DirectChatActor (out: ActorRef, actorSystem: ActorSystem) extends Actor {
+class DirectChatActor (out: ActorRef, actorSystem: ActorSystem, userId: String, directChatService: DirectChatService) extends Actor {
   def receive: PartialFunction[Any, Unit] = {
     case _: Unit => ()
+  }
+
+  override def preStart(): Unit = {
+    if(directChatService.isGivenPlayerInvalid(userId)) {
+      self ! PoisonPill
+    }
   }
 
   override def postStop(): Unit = {
