7a6abd9178599b1405459066885895ad90bd57bb
xenonso <jakubstanislawczyk98@gmail.com>
2019-05-27 11:22:34 +0200

    Serializer generic methods

--- app/serializer/JsonSerializer.scala
+++ app/serializer/JsonSerializer.scala
@@ -1,6 +1,16 @@
 package serializer
 
-trait JsonSerializer[Object] {
-  def toJson(model: Object): String
-  def fromJson(json: String): Object
+import com.google.gson.Gson
+
+trait JsonSerializer[T] {
+
+  protected val gson = new Gson
+
+  def toJson(t: T): String = {
+    gson.toJson(t)
+  }
+
+  def fromJson(json: String): T = {
+    gson.fromJson(json, classOf[Class[T]])
+  }
 }
