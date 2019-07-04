10ff2ed7a6e04efaa298721e208888ebe62ec109
xenonso <jakubstanislawczyk98@gmail.com>
2019-07-04 14:46:36 +0200

    New nested JSON responses for websocket messages

--- app/helper/WebSocketResponseType.scala
+++ app/helper/WebSocketResponseType.scala
@@ -2,5 +2,6 @@ package helper
 
 object WebSocketResponseType extends Enumeration {
   type WebSocketResponseType = Value
-  val DIRECT_CHAT = Value
+
+  val DIRECT_CHAT, GLOBAL_CHAT, MATCH_SAVED, MATCH_DELETED = Value
 }
