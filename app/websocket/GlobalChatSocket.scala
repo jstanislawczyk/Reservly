790488090abd943ddc3714bfd3f2972333882c0c
xenonso <jakubstanislawczyk98@gmail.com>
2019-09-27 10:46:47 +0200

    Using player id for global chat socket channel id

--- app/websocket/GlobalChatSocket.scala
+++ app/websocket/GlobalChatSocket.scala
@@ -13,8 +13,8 @@ import scala.concurrent.ExecutionContext
 
 @Api("GlobalChatSocket")
 class GlobalChatSocket @Inject()
-  (cc: MessagesControllerComponents)
-  (implicit ec: ExecutionContext, actorSystem: ActorSystem, mat: Materializer) extends MessagesAbstractController(cc) {
+(cc: MessagesControllerComponents)
+(implicit ec: ExecutionContext, actorSystem: ActorSystem, mat: Materializer) extends MessagesAbstractController(cc) {
 
   @ApiOperation(
     value = "Open global chat websocket connection",
@@ -23,15 +23,15 @@ class GlobalChatSocket @Inject()
   @ApiResponses(Array(
     new ApiResponse(code = 200, message = "Opens global chat websocket connection")
   ))
-  def globalChat: WebSocket = WebSocket.accept[String, String] { _ =>
+  def globalChat(playerId: String): WebSocket = WebSocket.accept[String, String] { _ =>
     ActorFlow.actorRef(out => {
-      registerNewActor(actorSystem, out.path.toString)
+      registerNewActor(actorSystem, out.path.toString, playerId)
       GlobalChatActor.props(out, actorSystem)
     })
   }
 
-  private def registerNewActor(actorSystem: ActorSystem, actorPath: String): Unit = {
+  private def registerNewActor(actorSystem: ActorSystem, actorPath: String, playerId: String): Unit = {
     val globalChatActorRegister = new GlobalChatActorRegister(actorSystem)
-    globalChatActorRegister.registerNewActor(actorPath)
+    globalChatActorRegister.registerNewActor(actorPath, playerId)
   }
 }
